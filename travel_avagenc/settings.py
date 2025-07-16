77d1506b758187644ae87992931f5ea5e774e3d7
VicoAritonang <chaikaaaaa5@gmail.com>
2025-07-16 10:59:02 +0700

    something

--- travel_avagenc/settings.py
+++ travel_avagenc/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'changeme-in-production')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['.onrender.com', '*']
 
 
 # Application definition
